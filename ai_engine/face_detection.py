1180b7bb9794fce4d75797c895a1946e037ef407
mohd-umair <umairofficial040@gmail.com>
2026-06-18 19:57:53 +0530

    UPDATED

--- ai_engine/face_detection.py
+++ ai_engine/face_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 
-mp_face = mp.solutions.face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
+mp_face = mp.solutions.face_detection
 face_detector = mp_face.FaceDetection(model_selection=0, min_detection_confidence=0.5)
 
 
